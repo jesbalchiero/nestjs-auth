32d186a8b73dffb06ebf2f6c81d1f30772c3414c
jesbalchiero <62852809+jesbalchiero@users.noreply.github.com>
2024-04-10 12:35:03 -0300

    feat(auth): implement `@CurrentUser()`

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { IsPublic } from './auth/decorators/is-public.decorator';
+import { CurrentUser } from './auth/decorators/current-user.decorator';
+import { User } from '@prisma/client';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @IsPublic()
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('me')
+  getMe(@CurrentUser() user: User) {
+    return user; // 'Info extracted from JWT';
   }
 }
